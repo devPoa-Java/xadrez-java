671ed9d328b9e54f4393d4de5f470a400910ad4f
Sandro M. dos Santos <sandromarista@gmail.com>
2025-07-17 16:34:27 -0300

    Implementing possible moves of King

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -14,7 +14,6 @@ public abstract class Piece {
 	}
 
 	public abstract boolean[][] possibleMoves();
-	
 
 	public boolean possibleMove(Position position) {
 		return possibleMoves()[position.getRow()][position.getColumn()];
